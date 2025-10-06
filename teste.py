4572443e2539023c88aba07887aae6a43e2a3dac
adriennelm <adriennelourencomonsores@gmail.com>
2025-10-06 15:21:00 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,4 +1,6 @@
 import streamlit as st
 
 st.write("Alô mundo")
-st.image("https://www.google.com/imgres?q=bob%20esponja&imgurl=https%3A%2F%2Fpalavracolhida.com%2Fwp-content%2Fuploads%2F2015%2F06%2Fbob-esponja.png&imgrefurl=https%3A%2F%2Fpalavracolhida.com%2F2015%2F06%2F17%2Fbob-esponja-e-o-infantil-no-adulto%2F&docid=8rdwosuCsWL59M&tbnid=HhPjRw_1eYSKJM&vet=12ahUKEwinxfGAl5CQAxXCGLkGHbmGMfoQM3oECCIQAA..i&w=510&h=370&hcb=2&ved=2ahUKEwinxfGAl5CQAxXCGLkGHbmGMfoQM3oECCIQAA")
+nome = st.text_input("digite o seu nome")
+if nome: 
+  st.write(nome.upper())
