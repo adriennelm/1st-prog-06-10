5f1ae2b8b92ff0b9adbb74270dc26118aa126ff7
adriennelm <adriennelourencomonsores@gmail.com>
2025-10-08 12:31:00 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -4,4 +4,5 @@ st.write("Oiee usuários do planeta Terra 🌎🌊🌫🌄")
 nome = st.text_input("Digite o seu username nesta galáxia🌌🪐... PS: sem ninguém ver!👀")
 if nome: 
   st.write(nome.upper() and " Você recebeu permissão de ocupar uma vaga pelo tempo de 1 vida humana, aproveite!")
-  st.image(caption="Bem-vindo ao planeta Terra 🌍", "https://share.google/FB4wgEM9eZHU1E6KZ")
+  st.image("https://share.google/FB4wgEM9eZHU1E6KZ"
+    caption="Bem-vindo ao planeta Terra 🌍")
