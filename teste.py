d47fe9fbc90d29530e7441a30318aad5d50d4430
adriennelm <adriennelourencomonsores@gmail.com>
2025-10-08 12:22:31 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -4,5 +4,5 @@ st.write("Oiee usuários do planeta Terra 🌎🌊🌫🌄")
 nome = st.text_input("Digite o seu username nesta galáxia🌌🪐... PS: sem ninguém ver!👀")
 if nome: 
   st.write(nome.upper())
-st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/The_Earth_seen_from_Apollo_17.jpg/600px-The_Earth_seen_from_Apollo_17.jpg", 
+  st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/The_Earth_seen_from_Apollo_17.jpg/600px-The_Earth_seen_from_Apollo_17.jpg", 
          caption="Bem-vindo ao planeta Terra 🌍")
