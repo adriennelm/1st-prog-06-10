9f25c6096e095883fb2e250767b796fae79386f2
adriennelm <adriennelourencomonsores@gmail.com>
2025-10-08 11:50:15 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.write("Alô mundo")
-nome = st.text_input("digite o seu nome")
+st.write("Oiee usuários do planeta Terra")
+nome = st.text_input("Digite o seu username nesta galáxia
+PS: sem ninguém ver! ")
 if nome: 
   st.write(nome.upper())
