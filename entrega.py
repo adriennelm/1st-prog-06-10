74017bae9cd36c9b7a169266a29e00a1afdd9f09
adriennelm <adriennelourencomonsores@gmail.com>
2025-10-13 15:42:50 -0300

    Update and rename entrega to entrega.py

--- entrega.py
+++ entrega.py
@@ -1,3 +1,6 @@
 import pandas as pd 
+import streamlit as st
+import plotly.express as px
 dataset = pd.read_csv('https://raw.githubusercontent.com/adriennelm/1st-prog-06-10/refs/heads/main/deputados_2022.csv')
 display(dataset)
++ código
