3c1198db03f8ea564875f14866cbe754c32cc6bc
adriennelm <adriennelourencomonsores@gmail.com>
2025-10-13 15:05:43 -0300

    Update 2ndprog.py

--- 2ndprog.py
+++ 2ndprog.py
@@ -1,6 +1,13 @@
 import pandas as pd
 import streamlit as st
+import plotly.express as px
 
 dataset = pd.read_csv("https://www.irdx.com.br/media/uploads/paises.csv")
 
-st.dataframe(dataset)
+fig = px.choropleth(dataset,
+                     locations=dataset['iso3'],
+                     color=dataset['longitude'],
+                     hover_name=dataset['name'])
+fig.update_layout(title='Mapa Coroplético dos paises',
+                  geo_scope='world')
+st.plotly_chart(fig, fig_use
